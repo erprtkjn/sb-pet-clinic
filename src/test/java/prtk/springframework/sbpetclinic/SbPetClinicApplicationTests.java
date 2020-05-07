f29e6f4fae158987f8c600003417b7bcd6c4a97a
Prateek Jain <jain.prateek904@gmail.com>
2020-05-08 00:07:59 +0200

    First commit

---
+++ src/test/java/prtk/springframework/sbpetclinic/SbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package prtk.springframework.sbpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
