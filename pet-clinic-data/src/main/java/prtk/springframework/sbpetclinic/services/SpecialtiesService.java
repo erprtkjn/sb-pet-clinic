edaa0887afc70ce6194598870a74677b46ad9572
Prateek Jain <jain.prateek904@gmail.com>
2020-05-22 18:01:06 +0200

    Create Specialty Map Service Impl, closes #31

---
+++ pet-clinic-data/src/main/java/prtk/springframework/sbpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package prtk.springframework.sbpetclinic.services;
+
+import prtk.springframework.sbpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
