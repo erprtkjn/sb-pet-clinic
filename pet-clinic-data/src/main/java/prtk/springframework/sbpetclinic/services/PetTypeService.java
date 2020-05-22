a8946467ec493275598e40f55698616ebff1fe36
Prateek Jain <jain.prateek904@gmail.com>
2020-05-22 15:15:36 +0200

    Create PetType Map Service Impl, closes #29

---
+++ pet-clinic-data/src/main/java/prtk/springframework/sbpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package prtk.springframework.sbpetclinic.services;
+
+import prtk.springframework.sbpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
