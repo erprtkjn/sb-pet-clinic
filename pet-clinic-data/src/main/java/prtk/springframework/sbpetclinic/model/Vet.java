8c15605ee4ee14982645ec0e95a87bd7e523f617
Prateek Jain <jain.prateek904@gmail.com>
2020-05-29 17:55:48 +0200

    Add project LOMBOK and Refactor, closes #51

--- pet-clinic-data/src/main/java/prtk/springframework/sbpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/prtk/springframework/sbpetclinic/model/Vet.java
@@ -1,10 +1,16 @@
 package prtk.springframework.sbpetclinic.model;
 
-import javax.naming.Name;
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -14,11 +20,4 @@ public class Vet extends Person{
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
