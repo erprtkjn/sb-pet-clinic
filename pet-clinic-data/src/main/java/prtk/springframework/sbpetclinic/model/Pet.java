6dc51626a8168186b7905871be579f072c57383f
Prateek Jain <jain.prateek904@gmail.com>
2020-06-18 00:40:20 +0200

    Write CRUD Tests for Pet Map Service

--- pet-clinic-data/src/main/java/prtk/springframework/sbpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/prtk/springframework/sbpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package prtk.springframework.sbpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -35,7 +36,9 @@ public class Pet extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
