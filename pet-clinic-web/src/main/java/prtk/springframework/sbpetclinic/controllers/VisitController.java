de9fcb009db750f4936b2a7a7c279e3fc322038c
Prateek Jain <jain.prateek904@gmail.com>
2020-06-18 00:20:11 +0200

    Pet visit Date validation failing, closes #64

--- pet-clinic-web/src/main/java/prtk/springframework/sbpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/prtk/springframework/sbpetclinic/controllers/VisitController.java
@@ -10,6 +10,8 @@ import prtk.springframework.sbpetclinic.services.PetService;
 import prtk.springframework.sbpetclinic.services.VisitService;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.util.Map;
 
 @Controller
@@ -24,8 +26,14 @@ public class VisitController {
     }
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder) {
+    public void dataBinder(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     /**
